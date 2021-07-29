412f07d0506121cfd6005eee188825b0367dbc52
chris De Groef <chris.degroef@gmail.com>
2021-07-29 11:04:13 +0200

    Laravel : CRUD - show exo1

---
+++ resources/views/pages/creationIngredients.blade.php
@@ -0,0 +1,20 @@
+@extends('template.template')
+
+@section('content')
+    <h1>Création d'ingrédients</h1>
+
+    <form action="" method="post">
+
+        <label for="">Nom de l'ingrédient :</label>
+        <input type="text" name="" id="">
+
+        <label for="">Quantité de l'ingrédient :</label>
+        <input type="text" name="" id="">
+    
+    </form>
+
+    
+
+    
+
+@endsection
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('template.template')
+
+@section('content')
+    <h1>Création d'ingrédients</h1>
+
+    <a href="{{route('create')}}">Créer</a>
+
+    
+
+@endsection
